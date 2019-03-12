2c39faa7f05f49a99a99671b7bba5fb6fe39ac98
meIIwin <48351952+meIIwin@users.noreply.github.com>
2019-03-12 16:58:55 +0500

    Update slark.lua

--- slark.lua
+++ slark.lua
@@ -1,108 +1,100 @@
-local slark = {}
+local Nyx = {}
+
+Nyx.Enabled = Menu.AddOptionBool({"Hero Specific", "Nyx Assassin"}, "Enabled", false)
+Nyx.Key = Menu.AddKeyOption({"Hero Specific", "Nyx Assassin"}, "Combo Key", Enum.ButtonCode.KEY_V)
+Nyx.Dagon = Menu.AddOptionBool({"Hero Specific", "Nyx Assassin"}, "Dagon", false)
+Nyx.Urn = Menu.AddOptionBool({"Hero Specific", "Nyx Assassin"}, "Urn or Spirit Vessel", false)
+Nyx.EtherealBlade = Menu.AddOptionBool({"Hero Specific", "Nyx Assassin"}, "Ethereal Blade", false)
+Nyx.ShivaGuard = Menu.AddOptionBool({"Hero Specific", "Nyx Assassin"}, "Shivas Guard", false)
+Nyx.Impale = Menu.AddOptionBool({"Hero Specific", "Nyx Assassin"}, "Impale")
+Nyx.ManaBurn = Menu.AddOptionBool({"Hero Specific", "Nyx Assassin"}, "Mana Burn")
+Nyx.SpikedCarapace = Menu.AddOptionBool({"Hero Specific", "Nyx Assassin"}, "Spiked Carapace")
+Nyx.Vendetta = Menu.AddOptionBool({"Hero Specific", "Nyx Assassin"}, "Vendetta")
+
+local enemy = nil
+local myHero = nil
+
+local dagon
+local urn
+local etherealblade
+local shivaguard
+
+Nyx.lastTick = 0
+
+local impale
+local manaburn
+local spikedcarapace
+local vendetta
+
+local sleep_after_cast = 0
+local sleep_after_attack = 0 
 
 
-slark.Enable = Menu.AddOptionBool({ "Hero Specific", "slark" }, "enable", false)
-slark.Ult = Menu.AddOptionBool({ "Hero Specific", "slark" }, "use ult in combo?", false)
-slark.Pounce = Menu.AddOptionBool({ "Hero Specific", "slark" }, "use pounce in combo?", false)
-slark.Dark = Menu.AddOptionBool({ "Hero Specific", "slark" }, "use dark_pact in combo?", false)
-slark.Rem = Menu.AddOptionBool({ "Hero Specific", "slark", "dark_pact" }, "use for remove effects?", false)
-slark.Battle_hunger = Menu.AddOptionBool({ "Hero Specific", "slark", "dark_pact" }, "modifier_axe_battle_hunger", false)
-slark.Enfeeble = Menu.AddOptionBool({ "Hero Specific", "slark", "dark_pact" }, "modifier_bane_enfeeble", false)
-slark.Poison_touch = Menu.AddOptionBool({ "Hero Specific", "slark", "dark_pact" }, "modifier_dazzle_poison_touch", false)
-slark.Call_down_slow = Menu.AddOptionBool({ "Hero Specific", "slark", "dark_pact" }, "modifier_gyrocopter_call_down_slow", false)
-slark.Cold_feet = Menu.AddOptionBool({ "Hero Specific", "slark", "dark_pact" }, "modifier_cold_feet", false)
-slark.Coldfeet_freeze = Menu.AddOptionBool({ "Hero Specific", "slark", "dark_pact" }, "modifier_ancientapparition_coldfeet_freeze", false)
-slark.Flux = Menu.AddOptionBool({ "Hero Specific", "slark", "dark_pact" }, "modifier_arc_warden_flux", false)
-slark.Void = Menu.AddOptionBool({ "Hero Specific", "slark", "dark_pact" }, "modifier_night_stalker_void", false)
-slark.Gale = Menu.AddOptionBool({ "Hero Specific", "slark", "dark_pact" }, "modifier_venomancer_venomous_gale", false)
-slark.Track = Menu.AddOptionBool({ "Hero Specific", "slark", "dark_pact" }, "modifier_bounty_hunter_track", false)
-slark.Glimpse = Menu.AddOptionBool({ "Hero Specific", "slark", "dark_pact" }, "modifier_disruptor_glimpse", false)
-slark.Key = Menu.AddKeyOption({ "Hero Specific", "slark" }, "Combo Key", Enum.ButtonCode.KEY_1)
-slark.Percent = Menu.AddOptionSlider({ "Hero Specific", "slark" }, "hp to ult", 1, 100, 20)
-slark.Abyssal = Menu.AddOptionBool({ "Hero Specific", "slark" }, "use abyssal in combo?", false)
-slark.Orchid = Menu.AddOptionBool({ "Hero Specific", "slark" }, "use orchid in combo?", false)
-slark.Bloothorn = Menu.AddOptionBool({ "Hero Specific", "slark" }, "use bloothorn in combo?", false)
-slark.Bkb = Menu.AddOptionBool({ "Hero Specific", "slark" }, "use bkb in combo?", false)
-slark.Nullifier = Menu.AddOptionBool({ "Hero Specific", "slark" }, "use nullifier in combo?", false)
 
-function slark.OnUpdate()
-	if not Menu.IsEnabled(slark.Enable) then
-        return
-    end
-    if not me or NPC.GetUnitName(me) ~= "npc_dota_hero_slark" then
-        return
-    end
-    if not Entity.IsAlive(me) or NPC.IsStunned(me) or NPC.IsSilenced(me) then
-        return
-    end
-
-
-  	if Menu.IsEnabled(slark.Enable) then slark.Combo() end 
+function Nyx.OnUpdate()
+    if not Menu.IsEnabled(Nyx.Enabled) or not Engine.IsInGame() or not Heroes.GetLocal() then return end
+	    myHero = Heroes.GetLocal()
+		if NPC.GetUnitName(myHero) ~= "npc_dota_hero_nyx_assassin" then return end
+	    if not Entity.IsAlive(myHero) or NPC.ISstunned(myHero) or NPC.IsSilenced(myHero) then return end
+		if Menu.IsKeyDown(Nyx.Key) then
+		         enemy = Input.GetNearestHeroToCursor(Entity.GetTeamNum(myHero), Enum.TeamType.TEAM_ENEMY)
+	             if enemy and enemy~= 0 then
+				         Nyx.Combo(myHero, enemy)
+						 return
+			     end
+		end
 end
 
-function slark.Combo()
-	target = nil
-	myTeam = Entity.GetTeamNum(me)
-	enemy = Input.GetNearestHeroToCursor(myTeam, Enum.TeamType.TEAM_ENEMY)
-	player =  Players.GetLocal()
-	me = Heroes.GetLocal()
-  	mana = NPC.GetMana(me)
-  	health = Entity.GetHealth(me)
-  	max_hp = Entity.GetMaxHealth(me)
-  	ult = NPC.GetAbility(me, "slark_shadow_dance")
-  	abyssal = NPC.GetItem(me, "item_abyssal_blade", true)
-  	bkb = NPC.GetItem(me, "item_black_king_bar")
-  	orchid = NPC.GetItem(me, "item_orchid")
-  	bloodthorn = NPC.GetItem(me, "item_bloodthorn", true)
-  	nullifier = NPC.GetItem(me, "item_nullifier")
-  	pounce = NPC.GetAbility(me, "slark_pounce")
-  	dark = NPC.GetAbility(me, "slark_dark_pact")
-  	dark_range = 700
-    cursor_pos = Input.GetWorldCursorPos()
-	
+function Nyx.Combo(myHero, enemy)
 
-	if ult and (max_hp*(Menu.GetValue(slark.Percent))/100 > health) and Ability.IsReady(ult) and Ability.IsCastable(ult, mana) then
-		Ability.CastNoTarget(ult) end
-	if Menu.IsEnabled(slark.Rem) and Menu.IsEnabled(slark.Battle_hunger) and dark and Ability.IsReady(dark) and Ability.IsCastable(dark, mana) and NPC.HasModifier(me, "modifier_axe_battle_hunger") then 
-		Ability.CastNoTarget(dark) end
-	if Menu.IsEnabled(slark.Rem) and Menu.IsEnabled(slark.Enfeeble) and dark and Ability.IsReady(dark) and Ability.IsCastable(dark, mana) and NPC.HasModifier(me, "modifier_bane_enfeeble") then 
-		Ability.CastNoTarget(dark) end
-	if Menu.IsEnabled(slark.Rem) and Menu.IsEnabled(slark.Poison_touch) and dark and Ability.IsReady(dark) and Ability.IsCastable(dark, mana) and NPC.HasModifier(me, "modifier_dazzle_poison_touch") then 
-		Ability.CastNoTarget(dark) end
-	if Menu.IsEnabled(slark.Rem) and Menu.IsEnabled(slark.Call_down_slow) and dark and Ability.IsReady(dark) and Ability.IsCastable(dark, mana) and NPC.HasModifier(me, "modifier_gyrocopter_call_down_slow") then 
-		Ability.CastNoTarget(dark) end
-	if Menu.IsEnabled(slark.Rem) and Menu.IsEnabled(slark.Cold_feet) and dark and Ability.IsReady(dark) and Ability.IsCastable(dark, mana) and NPC.HasModifier(me, "modifier_cold_feet") then 
-		Ability.CastNoTarget(dark) end
-	if Menu.IsEnabled(slark.Rem) and Menu.IsEnabled(slark.Coldfeet_freeze) and dark and Ability.IsReady(dark) and Ability.IsCastable(dark, mana) and NPC.HasModifier(me, "modifier_ancientapparition_coldfeet_freeze") then 
-		Ability.CastNoTarget(dark) end
-	if Menu.IsEnabled(slark.Rem) and Menu.IsEnabled(slark.Flux) and dark and Ability.IsReady(dark) and Ability.IsCastable(dark, mana) and NPC.HasModifier(me, "modifier_arc_warden_flux") then 
-		Ability.CastNoTarget(dark) end
-	if Menu.IsEnabled(slark.Rem) and Menu.IsEnabled(slark.Void) and dark and Ability.IsReady(dark) and Ability.IsCastable(dark, mana) and NPC.HasModifier(me, "modifier_night_stalker_void") then 
-		Ability.CastNoTarget(dark) end
-	if Menu.IsEnabled(slark.Rem) and Menu.IsEnabled(slark.Gale) and dark and Ability.IsReady(dark) and Ability.IsCastable(dark, mana) and NPC.HasModifier(me, "modifier_venomancer_venomous_gale") then 
-		Ability.CastNoTarget(dark) end
-	if Menu.IsEnabled(slark.Rem) and Menu.IsEnabled(slark.Track) and dark and Ability.IsReady(dark) and Ability.IsCastable(dark, mana) and NPC.HasModifier(me, "modifier_bounty_hunter_track") then 
-		Ability.CastNoTarget(dark) end
-	if Menu.IsEnabled(slark.Rem) and Menu.IsEnabled(slark.Glimpse) and dark and Ability.IsReady(dark) and Ability.IsCastable(dark, mana) and NPC.HasModifier(me, "modifier_disruptor_glimpse") then 
-		Ability.CastNoTarget(dark) end
-	if Menu.IsKeyDown(slark.Key) then
-		Player.PrepareUnitOrders(Players.GetLocal(), Enum.UnitOrder.DOTA_UNIT_ORDER_MOVE_TO_POSITION, enemy, Input.GetWorldCursorPos(), nil, Enum.PlayerOrderIssuer.DOTA_ORDER_ISSUER_HERO_ONLY, me)
-		Player.AttackTarget(player, me, enemy)
-		if Menu.IsEnabled(slark.Abyssal) and abyssal and Ability.IsCastable(abyssal, mana) and Ability.IsReady(abyssal) then Ability.CastTarget(abyssal, enemy) end
-		if Menu.IsEnabled(slark.Ult) and ult and Ability.IsCastable(ult, mana) and Ability.IsReady(ult) then Ability.CastNoTarget(ult) end
-		if Menu.IsEnabled(slark.Bkb) and bkb and Ability.IsCastable(bkb, mana) and Ability.IsReady(bkb) then Ability.CastNoTarget(bkb) end
-		if Menu.IsEnabled(slark.Orchid) and orchid and Ability.IsCastable(orchid, mana) and Ability.IsReady(orchid) then Ability.CastTarget(orchid, enemy) end
-		if Menu.IsEnabled(slark.Nullifier) and nullifier and Ability.IsCastable(nullifier, mana) and Ability.IsReady(nullifier) then Ability.CastTarget(nullifier, enemy) end
-		if Menu.IsEnabled(slark.Bloothorn) and bloodthorn and Ability.IsCastable(bloodthorn, mana) and Ability.IsReady(bloodthorn) then Ability.CastTarget(bloodthorn, enemy, true) end
-		if Menu.IsEnabled(slark.Pounce) and pounce and NPC.IsPositionInRange(me, Entity.GetAbsOrigin(enemy), dark_range) and Ability.IsCastable(pounce, mana) and Ability.IsReady(pounce) then 
-			Player.AttackTarget(player, me, enemy)
-			Ability.CastNoTarget(pounce) end
-		if Menu.IsEnabled(slark.Dark) and dark and Ability.IsCastable(dark, mana) and Ability.IsReady(dark) then 
-			Ability.CastNoTarget(dark) end
-	end
-	
-	
-
+     impale = NPC.GetAbility(myHero, "nyx_assassin_impale")
+     burn = NPC.GetAbility(myHero, "nyx_assassin_mana_burn")
+     spiked = NPC.GetAbility(myHero, "nyx_assassin_spiked_carapace")
+	 
+	 dagon = NPC.GetItem(myHero,"item_dagon") 
+	 urn = NPC.GetItem(myHero,"item_urn_of_shadows")
+    if not urn then
+        urn = NPC.GetItem(myHero, "item_spirit_vessel")
+    end
+	 blade = NPC.GetItem(myHero, "item_ethereal_blade")
+	 shiva = NPC.GetItem(myHero, "item_shivas_guard")
+	 
+	 local myMana = NPC.GetMana(myHero)
+	 
+	 
+	 if Menu.IsKeyDown(Nyx.Key) and Entity.GetHealth(enemy) > 0 then  
+	     if not NPC.IsEntityInRange(myHero, enemy, 1500) then return end
+		 local enemy_origun = Entity.GetAbsOrigin(enemy)
+		 local curdor_pos = Input.GetWorldCursorPos()
+	     if (cursor_pos - enemy_origin):Length2D() > Menu.GetValue(Nyx.NearestTarget) then enemy = nil return end
+	     
+		 if impale and Ability.IsCastable(impale, myMana) and Menu.IsEnabled(Nyx.Impale) then
+		     Ability.CastPosition(impale, enemy_origin)
+			 Nyx.lastTick = os.clock()
+	     end
+			 
+		 if urn and Ability.IsReady(urn) and Menu.IsEnabled(Nyx.Urn) then
+            Ability.CastTarget(urn, enemy)
+         end
+		 
+		 if blade and Ability.IsReady(blade, myMana) and Menu.IsEnabled(Nyx.Blade) then
+            Ability.CastTarget(blade, enemy)
+         end
+		 
+		 if dagon and Ability.IsReady(dagon, myMana) and Menu.IsEnabled(Nyx.Dagon) then
+		     Ability.CastTarget(dagon, enemy)
+		 end
+		 
+		  if shiva and Ability.IsReady(shiva, myMana) and Menu.IsEnabled(Nyx.Shiva) then
+            Ability.CastNoTarget(shiva)
+        end
+    end
 end
-return slark
+
+
+
+return Nyx
+
+
+
 
